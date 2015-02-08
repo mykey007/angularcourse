1137fb53ed6375f754688f0ee4295b94d82d35d2
mykey007 <mmallowe@gmail.com>
2015-02-07 22:30:20 -0800

    party info and text messages differenct documents, phone number

--- js/controllers.js
+++ js/controllers.js
@@ -7,18 +7,28 @@ angular.module('myApp.controllers', [])
 
 	}])
 	.controller('WaitListController', ['$scope', '$firebase', function($scope, $firebase){
-		var partiesRef = new Firebase('https://dazzling-fire-3767.firebaseio.com/');
+		// connecting $scope.parties to live firebase data
+		var partiesRef = new Firebase('https://dazzling-fire-3767.firebaseio.com/parties');
 
 		//scope is an object that html and js share
 		//collect data from the form and store it in an array
 		$scope.parties = $firebase(partiesRef);
 
-		$scope.party = {name: '', phone: '', size: ''};
+		// object to store data from waitlist form
+		$scope.newParty = {name: '', phone: '', size: ''};
 
+		// function to save a new party to the waitlist
 		$scope.saveParty = function(){
 			//take party object and put it in the parties array
-			$scope.parties.$add($scope.party);
-			$scope.party = {name: '', phone: '', size: ''};
+			$scope.parties.$add($scope.newParty);
+			$scope.newParty = {name: '', phone: '', size: ''};
+		};
+
+		//function to send text message to a party
+		$scope.sendTextMessage = function(phoneNumber){
+			//add object to db
+			var textMessageRef = new Firebase('https://dazzling-fire-3767.firebaseio.com/textMessages');
+			var textMessages = $firebase(textMessageRef);
+			textMessages.$add({phoneNumber: phoneNumber});
 		};
 	}]);
-
